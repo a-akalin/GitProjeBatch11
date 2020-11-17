12df9d91a60d3dddb50b258563495634986946c3
a-akalin <ahmet.akalin99@gmail.com>
2020-11-17 23:12:53 +0300

    mesaj

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -6,6 +6,6 @@ public class BirinciGun {
 		System.out.println("10:11- ikinci Gun");
 		System.out.println("10:26- ikinci Gun");
 		System.out.println("10:35-ikinci gun ");
-		System.out.println("11:05-github.com ");
+		System.out.println("11:05-local ");
 	}
 }
