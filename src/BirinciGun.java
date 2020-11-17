a328e432f6d2b071a9b4f149edf6c1947b438b6a
a-akalin <62880117+a-akalin@users.noreply.github.com>
2020-11-17 22:15:12 +0300

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -1,6 +1,8 @@
 
 public class BirinciGun {
 	public static void main(String[] args) {
-		System.out.println("11:05-Birinci Gun");
+		System.out.println("11:57-Birinci Gun");
+		System.out.println("İkinci Commit");
+		System.out.println("10:11- İkinci Gün");
 	}
 }
