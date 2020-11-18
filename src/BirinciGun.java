9975d114a6818a4b3311bd43bf19f13c9d77e7a9
a-akalin <ahmet.akalin99@gmail.com>
2020-11-18 23:21:37 +0300

    mesaj

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -9,5 +9,6 @@ public class BirinciGun {
 		System.out.println("11:05-local ");
 		System.out.println("11.23-github.com");
 		System.out.println("10:26-feature branch");
+		System.out.println("11:21-feature branch");
 	}
 }
